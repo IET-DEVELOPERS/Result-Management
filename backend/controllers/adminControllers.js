784603e28c3f07996d4bdda246ccddaf9ab64360
snehil <visputesnehil2002@gmail.com>
2022-11-04 23:49:10 +0530

    better admin code!!

--- backend/controllers/adminControllers.js
+++ backend/controllers/adminControllers.js
@@ -1,11 +1,37 @@
 const Admin=require('../models/adminModel');
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler');
+const bcrypt=require('bcryptjs');
 const registerAdmin=asyncHandler ( async (req,res)=>{
-        const admin=await Admin.create(req.body);
-        if(admin){
-        res.status(201).json(admin);
-        }else{
-            throw new Erro("cant create admin!!");
-        }      
+    
+    let name=req.body.name
+    let email=req.body.email
+    let password=req.body.password
+    
+    if(!name || !email  || !password){
+        res.status(400);
+        throw new Error("Please Enter all the fields");
+    }
+
+    const adminExists=await Admin.findOne({email:email});
+    if(adminExists){
+        res.status(400);
+        throw new Error("admin already exists");
+    }
+
+    const salt=await bcrypt.genSalt(10);
+    password=await bcrypt.hash(password,salt);
+
+   const admin=await Admin.create({
+    name,
+    email,
+    password
+   });
+
+   if(admin){
+    res.status(201).json(admin);
+   }else{
+        res.status(400);
+        throw new Error("Cant Create Admin");
+   }
 })
 module.exports={registerAdmin};
