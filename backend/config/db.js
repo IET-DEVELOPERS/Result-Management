23c78d304d269d62431fbb580c1049f7f8a283e2
TanmayTr911 <89075656+TanmayTr911@users.noreply.github.com>
2023-01-03 12:26:20 +0530

    env added

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(
-      "mongodb+srv://ietdevelopers:iet111911@cluster0.hhmimpq.mongodb.net/?retryWrites=true&w=majority"
-    );
+    const connection = await mongoose.connect(process.env.MONGOURI);
     console.log("mongoose connected successfully!!");
   } catch (err) {
     console.log(err);
